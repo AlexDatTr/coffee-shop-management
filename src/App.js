7aedf0cdd169cdc8780384f2f997393d8887f3fc
Alex (Dat Tran) Tran <134574511+AlexDatTr@users.noreply.github.com>
2024-11-12 20:55:11 -0500

    first_commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Recipes from './pages/Recipes';
+import Sales from './pages/Sales.js';
+import Orders from './pages/Orders';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/recipes" element={<Recipes />} />
+          <Route path="/sales" element={<Sales />} />
+          <Route path="/orders" element={<Orders />} />
+        </Routes>
+      </Router>
+      
+      <div className="App">
+        <header className="App-header">
+          <h1>Hello, Coffee Shop!</h1>
+        </header>
+      </div>
+    </>
   );
 }
 
